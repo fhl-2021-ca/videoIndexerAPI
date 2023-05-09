e393eafb1e5ca18bd412e83b6c9a809ca038aa6b
FAREAST\aseemgoyal <aseemgoyal@microsoft.com>
2023-05-09 16:37:46 +0530

    Working API

---
+++ FHLMarch2023/WebApplication1/Controllers/.vshistory/WeatherForecastController.cs/2023-05-09_11_58_18_899.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VideoIndexerArm;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<string> GetAsync(String videoUrl)
+        {
+            var rng = new Random();
+            String result = await VideoProgram.indexvideoAndGetInsights(videoUrl);
+            return videoUrl;
+
+
+
+/*            return new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(0),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            };
+*/        }
+    }
+}
